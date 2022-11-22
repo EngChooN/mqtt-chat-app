b1458ee50c98e0014aeb657fe5d2d90baf43826f
Joonyoung Cho <aimzero@vertigogames.com>
2022-11-22 18:49:44 +0900

    mqtt연결작업중..

--- components/chatbox/Chats.tsx
+++ components/chatbox/Chats.tsx
@@ -1,9 +1,33 @@
+import { useEffect, useState } from "react";
 import { ChatWrapper, ChatBox } from "./Chats.styles";
+import mqtt from "mqtt";
 
 export default function Chat() {
+  const [userName, setUserName] = useState("");
+  const [userUrl, setUserUrl] = useState("");
+  const [userPort, setUserPort] = useState("");
+
+  // 로컬스토리지에서 데이터를 가져옴
+  useEffect(() => {
+    setUserName(localStorage.getItem("username"));
+    setUserUrl(localStorage.getItem("userurl"));
+    setUserPort(localStorage.getItem("userport"));
+  });
+
   return (
     <ChatWrapper>
-      <ChatBox>d</ChatBox>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          marginBottom: "20px",
+        }}
+      >
+        환영합니다!!{" "}
+      </div>
+      <ChatBox>
+        ddasdjsaijdiasjdisajdksajdsalkjdsakjdskajdkasjfhfuwhfgusadhgsasopdksadksapodksapokdsapodksapodkspoakdospakdsoapkpdjkfblkdjshfdlksajf
+      </ChatBox>
     </ChatWrapper>
   );
 }
